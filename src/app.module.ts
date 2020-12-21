b9c71d1868619ffff7f025bdc6b5960c7fe13fbc
zzoon <188zzoon@gmail.com>
2020-12-22 06:09:33 +0900

    2.6 Validating ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
+import * as Joi from "joi";
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { RestaurantsModule } from './restaurants/restaurants.module';
+
+// console.log(`ENV : ${process.env.NODE_ENV}`)
+console.log(Joi)
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : ".env.test",
-      ignoreEnvFile: process.env.NODE_ENV === 'prod'
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+        .valid('dev', 'prod')
+        .required(),
+        DB_HOST : Joi.string().required(),
+        DB_PORT : Joi.string().required(),
+        DB_USERNAME : Joi.string().required(),
+        DB_PASSWORD : Joi.string().required(),
+        DB_NAME : Joi.string().required(),
+      })
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -31,7 +45,4 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
   providers: [],
 })
 export class AppModule {
-  constructor(){
-    console.log(`ENV : ${process.env.NODE_ENV}`)
-  }
 }
