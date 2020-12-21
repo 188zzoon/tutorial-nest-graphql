1574f1b4450d729abb6333b5acbe0f513e7b2685
zzoon <188zzoon@gmail.com>
2020-12-22 05:42:03 +0900

    2.3 TypeORM Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from "@nestjs/graphql";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '165.246.43.140',
+      port: 5432,
+      username: 'postgres',
+      password: 'dev4481',
+      database: 'nuber-eats',
+      entities: [],
+      synchronize: true,
+			logging: true
+    }),
+
     GraphQLModule.forRoot({
       autoSchemaFile: true
     }), 
