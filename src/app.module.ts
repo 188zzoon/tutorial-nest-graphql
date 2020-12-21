b9b59281db1c79e85ef46cb1b4156fdc303ced48
zzoon <188zzoon@gmail.com>
2020-12-22 06:13:45 +0900

    3.0 Our First Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,7 +32,7 @@ console.log(Joi)
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
 			logging: true
     }),
     GraphQLModule.forRoot({
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { Column, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
 export class Restaurant {
+
+    @PrimaryGeneratedColumn()
+    @Field(type => Number)
+    id: number;
+
     @Field(type => String)
+    @Column()
     name: string;
 
     @Field(type => String)
+    @Column()
     isVegan: boolean;
 
     @Field(type => String)
+    @Column()
     address: string;
 
     @Field(type => String)
+    @Column()
     ownerName: string;
+
+    @Field(type => String)
+    @Column()
+    categoryName: string
 }
