6c06e866f4f0e4398c70fbad2fab6156c25531db
zzoon <188zzoon@gmail.com>
2020-12-22 04:23:39 +0900

    1.1 ObjectType

--- src/resturants/resturants.resolver.ts
+++ src/resturants/resturants.resolver.ts
@@ -1,10 +1,11 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from "./entities/restaurant.entity";
 
 @Resolver()
 export class ResturantsResolver {
     
-    @Query(returns => Boolean)
-    isPizzaGood(): Boolean {
+    @Query(returns => Restaurant)
+    myRestaurant() {
         return true
     }
 }
