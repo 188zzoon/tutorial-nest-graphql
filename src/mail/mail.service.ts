5680caba4417d2b6b54eb41aa87dc471c31dcb6a
188zzoon <46486231+188zzoon@users.noreply.github.com>
2021-01-05 14:53:04 +0900

    6.7 Mailgun API

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,7 +1,8 @@
+import got from 'got'
+import * as FormData  from "form-data";
 import { Inject, Injectable } from "@nestjs/common";
 import { CONFIG_OPTIONS } from "src/common/common.constants";
 import { MailModuleOptions } from "./mail.interfaces";
-
 
 @Injectable()
 export class MailService{
@@ -12,25 +13,28 @@ export class MailService{
     }
 
     private async sendMail(subject: string, content: string) {
-        const response = await fetch(
+        const form = new FormData();
+        form.append('from', `Excited User <mailgun@${this.options.domain}>`),
+        form.append('to', `zzoon.dev@gmail.com`)
+        form.append('subject', subject)
+        form.append('text', content)
+        const response = await got(
             `https://api.mailgun.net/v3/${this.options.domain}/messages`,
             {
                 method: 'POST',
                 headers: {
-                    Authentication: `Basic ${Buffer.from(
-                        `api:${this.options.apiKey}`,
-                    ).toString('base64')}`,
+                    Authrization: `Basic ${Buffer.from(`api:${this.options.apiKey}`,).toString('base64')}`,
                 },
-                body: JSON.stringify({
-                    from: this.options.fromEmail,
-                    to: 'zoon.dev@gamil.com',
-                    subject: subject,
-                    text: content
-                })
+                // body: JSON.stringify({
+                //     from: this.options.fromEmail,
+                //     to: 'zoon.dev@gamil.com',
+                //     subject: subject,
+                //     text: content
+                // })
+                body: form
             }
         );
-        const json = await response.json()
-        console.log(json)
+        console.log(response.body)
         console.log("=======TEST===========")
     }
 }
